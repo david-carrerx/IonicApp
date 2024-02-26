6823fda97d8a093ec18a1b714bbfa464526625f5
Ángel David Lerma Carrera <angeldavidlermacarrera@Angels-MacBook-Air.local>
2024-02-26 09:02:07 -0600

    Metodo post para el registro de usuarios

--- app/src/app/register/register.page.ts
+++ app/src/app/register/register.page.ts
@@ -1,73 +1,36 @@
-import { Component, OnInit } from '@angular/core';
-import { User } from '../modules/user.model';
-import { AngularFireAuth } from '@angular/fire/compat/auth';
-import { NavController } from '@ionic/angular';
-import { ToastController } from '@ionic/angular';
-import { LoadingController } from '@ionic/angular';
+import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.page.html',
   styleUrls: ['./register.page.scss'],
 })
-export class RegisterPage implements OnInit {
+export class RegisterPage {
+  name: string | undefined;
+  email: string | undefined;
+  password: string | undefined; 
+  phone: string | undefined;
+  role: string | undefined;
 
-  user = {} as User;
+  constructor(private http: HttpClient) {}
 
-  constructor(
-    private toastController: ToastController,
-    private loadingController: LoadingController,
-    private afAuth: AngularFireAuth,
-    private navCtrl: NavController) {
+  register() {
+    const userData = {
+      name: this.name,
+      email: this.email,
+      password: this.password,
+      phone: this.phone,
+      role: this.role
+    };
+
+    this.http.post('http://localhost/api/post.php', userData)
+      .subscribe((response) => {
+        console.log('Registro exitoso', response);
+        // Aquí podrías manejar la respuesta del backend, como mostrar un mensaje de éxito al usuario
+      }, (error) => {
+        console.error('Error al registrar', error);
+        // Aquí podrías manejar el error, como mostrar un mensaje de error al usuario
+      });
   }
-
-  ngOnInit() {
-  }
-
-  async register (user:User){
-    if(this.formValidation()){
-      let loader = await this.loadingController.create({
-        message: "Loading"
-      })
-
-      await loader.present();
-
-      try {
-        await this.afAuth.createUserWithEmailAndPassword(user.email, user.password).then(data =>{
-          console.log(data);
-
-          this.navCtrl.navigateRoot("home")
-        })
-        
-      } catch (e:any) {
-        e.message = "Failed";
-        let errorMessage = e.message || e.getLocalizedMessage();
-
-        this.showToast(errorMessage);
-      }
-
-      await loader.dismiss();
-    }
-  }
-
-  formValidation(){
-    if(!this.user.email){
-      this.showToast("Insert an email");
-      return false;
-    }
-    if(!this.user.password){
-      this.showToast("Insert a password");
-      return false;
-    }
-
-    return false;
-  }
-
-  showToast(message: string){
-    this.toastController.create({
-      message: message,
-      duration: 4000
-    }).then(toastData => toastData.present());
-  }
-
 }
