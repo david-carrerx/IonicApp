337ca0843fd1ff9d1f8dd206fbf1229a32689ac3
David Carrera <142541347+david-carrerx@users.noreply.github.com>
2024-03-07 14:20:24 -0600

    Componente footer

--- app/src/app/components/components.module.ts
+++ app/src/app/components/components.module.ts
@@ -1,13 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CardComponent } from './card/card.component';
+import { FooterComponent } from './footer/footer.component';
 
 
 @NgModule({
-  declarations: [CardComponent],
+  declarations: [CardComponent, FooterComponent],
   imports: [
     CommonModule
   ],
-  exports: [CardComponent]
+  exports: [CardComponent, FooterComponent]
 })
 export class ComponentsModule { }
--- app/src/app/user/user.module.ts
+++ app/src/app/user/user.module.ts
@@ -1,19 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-
 import { IonicModule } from '@ionic/angular';
-
 import { UserPageRoutingModule } from './user-routing.module';
-
 import { UserPage } from './user.page';
+import { ComponentsModule } from '../components/components.module';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    UserPageRoutingModule
+    UserPageRoutingModule,
+    ComponentsModule
   ],
   declarations: [UserPage]
 })
